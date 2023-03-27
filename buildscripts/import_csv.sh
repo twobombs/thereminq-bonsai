df1e73a8c27948750d5b96c749799a2cf7a5ee71
Aryan Blaauw <ablaauw@gmx.net>
2023-03-27 19:10:15 +0200

    Update import_csv.sh

--- buildscripts/import_csv.sh
+++ buildscripts/import_csv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cat *.csv | sed 's/[^0-9]*//g' | tr ',,' '' | tr ',,' '' | sed 's/|,|/\n/g' | split -d -n l/11 --additional-suffix='.dec' file csvoutput
+cat *.csv | sed 's/[^0-9]*//g' | tr ',,' '' | sed 's/|,|/\n/g' | split -d -n l/11 --additional-suffix='.dec' file csvoutput
 
