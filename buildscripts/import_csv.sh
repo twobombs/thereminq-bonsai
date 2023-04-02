1771cf40de2326e2d9f034a50608b97534ce7c30
Aryan Blaauw <ablaauw@gmx.net>
2023-04-02 13:59:42 +0200

    Update import_csv.sh

--- buildscripts/import_csv.sh
+++ buildscripts/import_csv.sh
@@ -17,7 +17,7 @@ awk '{print $11}' csvoutput.dec | tr " " "\n" > csvoutput11.dec
 awk '{print $12}' csvoutput.dec | tr " " "\n" > csvoutput12.dec
 awk '{print $13}' csvoutput.dec | tr " " "\n" > csvoutput13.dec
 
-# pad SDRP value in row 0 to 3 decs to make y value sane
+# pad SDRP value in row 0 to 3 decs to make x value sane
 for a in $(< csvoutput0.dec); do printf "000\r%s\n" $a ; done > csvoutput1.dec
 
 # convert 13x32k points to hex
@@ -40,10 +40,6 @@ sleep 10
 # checks
 wc -l csvoutput*
 
-paste csvoutput1.flex csvoutput2.flex  csvoutput1.flex csvoutput3.flex  csvoutput1.flex csvoutput4.flex  csvoutput1.flex csvoutput5.flex  csvoutput1.flex csvoutput6.flex  csvoutput1.flex csvoutput7.flex  csvoutput1.flex csvoutput8.flex  csvoutput1.flex csvoutput9.flex  csvoutput1.flex csvoutput10.flex  csvoutput1.flex csvoutput11.flex  csvoutput1.flex csvoutput12.flex  csvoutput1.flex csvoutput13.flex | sed 's/\t\t*/\n/g' > measured.flex
-
-wc -l measured.flex
-
 # ==== here starts tipsy header declarations ======
 # T = time
 echo "0000000000000000" > time.hex
@@ -53,13 +49,23 @@ echo "00000000" > ndark.hex
 echo "00010000" > version.hex
 # ==== here end tipsy special declarations =========
 
-# count the total amount of lines with values
+# count the total amount of lines for local ans all values
 wc -l measured.flex | tr " " "\n"| grep -v measured.flex > rows.dec
+wc -l csvoutput1.dec tr " " "\n"| grep -v collumns.dec > collumns.dec
+
+# create collumn coordinades in flex
+for i in `cat square.dec` ; do cat 
 
 # derive the amount of measured points for array dimension declaration
 rows=$(<rows.dec)
 echo $(( rows / 2 )) > points.dec
 printf '%08X\n' $(< points.dec) > points.hex
+
+# add velocity nullpointers for xyz displacements in hex/flex
+yes 00000000 | head -n `cat points.dec` > displacex.hex
+cp displacex.hex displacey.hex
+cp displacex.hex displacez.hex
+cp displacex.hex dummy.hex
 
 # dim a square by making a real sqr of the amount of points with margin
 points=$(<points.dec)
@@ -74,9 +80,19 @@ echo $square > cube.dec
 # announce and declare raw points
 # make real and tipsy scaling 
 # cube dimensions and convert value to hex for header
-
 echo "original amount of measured values" $points
 echo "view will be "$square "x" $square
 
+# create y coordinates in dec/flex for stacking teh layercaek
+collumns=$(<collumns.dec)
+seq 1 1 13 > collumnsy.dec
+for a in $(< collumsy.dec); do /root/.local/bin/crackNum -f sp a$ ; done > collumnsy.flex
+for a in $(< collumsy.flex); do yes $a | head -n $collumns > collumns${a}.flex
+
 # construct hex header
 paste time.hex points.hex ndim.hex nsph.hex ndark.hex points.hex version.hex > header.hex
+
+# create flex compatible file
+paste csvoutput1.flex csvoutput2.flex  csvoutput1.flex csvoutput3.flex  csvoutput1.flex csvoutput4.flex  csvoutput1.flex csvoutput5.flex  csvoutput1.flex csvoutput6.flex  csvoutput1.flex csvoutput7.flex  csvoutput1.flex csvoutput8.flex  csvoutput1.flex csvoutput9.flex  csvoutput1.flex csvoutput10.flex  csvoutput1.flex csvoutput11.flex  csvoutput1.flex csvoutput12.flex  csvoutput1.flex csvoutput13.flex | sed 's/\t\t*/\n/g' > measured.flex
+# ... and checks
+wc -l measured.flex
